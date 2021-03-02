29f2b385b4ce94530498034847b8effc44fd2f38
Michael <kd0azt@gmail.com>
2021-03-02 12:33:29 -0800

    Added base framework for the movesRemaining method.  Commented file for readability.

--- Sudoku/src/com/sudoku/models/User.java
+++ Sudoku/src/com/sudoku/models/User.java
@@ -33,11 +33,22 @@ public class User {
         return userScore;
     
     }
-    
-    public movesRemaining() {
-    
-       //the goal of this class is to analyze the current values in the column, row, and subgrid of the selected box and compute the possible values that would satisfy them.
-    
+    //to find moves remaining, take the row, column, and subboard values as arrays; combine them into one; and find which of 1-9 are missing.
+    public movesRemaining(subboard[] arr, line[] arr2, row[] arr3) {
+    	
+	int[] temp = new int[(arr.length + arr2.length + arr3.length)];
+	int i = 0;
+	
+	for (i = 0; i < arr.length; i++) {
+		temp[i] = arr[i];
+	}
+	for (i = 0; i < arr2.length; i++) {
+		temp[(i + arr.length)] = arr2[i];
+	}
+	for (i = 0; i < arr3.length; i++) {
+		temp[(i + arr.length + arr2.length)] = arr3[i];
+	}	
+    	//todo: analyze the arrays for the missing values.
     }
     
 }
