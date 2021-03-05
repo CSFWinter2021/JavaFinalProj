dc4b45f7c9862ab12e42b012a78222859db0a348
Michael <kd0azt@gmail.com>
2021-03-04 22:03:39 -0800

    Added to Users.java: Function for identifying which numbers are in the column/row/subboard to detwrmine available moves

--- Sudoku/src/com/sudoku/models/User.java
+++ Sudoku/src/com/sudoku/models/User.java
@@ -38,7 +38,7 @@ public class User {
     	
 	int[] temp = new int[(arr.length + arr2.length + arr3.length)];
 	int i = 0;
-	
+
 	for (i = 0; i < arr.length; i++) {
 		temp[i] = arr[i];
 	}
@@ -48,7 +48,50 @@ public class User {
 	for (i = 0; i < arr3.length; i++) {
 		temp[(i + arr.length + arr2.length)] = arr3[i];
 	}	
-    	//todo: analyze the arrays for the missing values.
+    	for (i = 0; i < temp.length; i++) {
+		if (temp[i] == 1) {
+			one = true;
+		}
+		else if (temp[i] == 2) {
+			two = true;
+		}
+		else if (temp[i] == 3) {
+			three = true;
+		}
+		else if (temp[i] == 4) {
+			four = true;
+		}
+		else if (temp[i] == 5) {
+			five = true;
+		}
+		else if (temp[i] == 6) {
+			six = true;
+		}
+		else if (temp[i] == 7) {
+			seven = true;
+		}
+		else if (temp[i] == 8) {
+			eight = true;
+		}
+		else {
+			nine = true;
+		}
+	}
     }
+    //create an array of values for 1-9 and store whether they are found in the row/column/subboard
+    public static void knownValues(int[] temp) {
     
+        int i;
+	int j;
+	boolean[] exists = {false, false, false, false, false, false, false, false, false};
+
+	for (i = 0; i < exists.length; i++) {
+		for (j = 0; j < temp.length; j++) {
+			if (temp[j] == i + 1) {
+				exists[i] = true;
+			}
+		}
+	}
+    
+    }
 }
