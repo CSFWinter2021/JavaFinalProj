b34d9e43e7c72fc528ed2adb540d5c456434a56b
KD0AZT <kd0azt@gmail.com>
2021-03-09 10:34:10 -0800

    User.java will now compile

--- Sudoku/src/com/sudoku/models/User.java
+++ Sudoku/src/com/sudoku/models/User.java
@@ -33,42 +33,44 @@ public class User {
         return userScore;
     
     }
-    //to find moves remaining, take the row, column, and subboard values as arrays; combine them into one; and find which of 1-9 are missing.
-    public void movesRemaining(subboard[] arr, line[] arr2, row[] arr3) {
-    	
-	int[] temp = new int[(arr.length + arr2.length + arr3.length)];
-	int i;
-	int j;
-	boolean[] exists = {false, false, false, false, false, false, false, false, false};
+
     
-    //copy the subboard values to a temp array.
-	for (i = 0; i < arr.length; i++) {
-		temp[i] = arr[i];
-	}
-	
-	//add the column values to the temp array.
-	for (i = 0; i < arr2.length; i++) {
-		temp[(i + arr.length)] = arr2[i];
-	}
-	
-	//add the row values to the temp array.
-	for (i = 0; i < arr3.length; i++) {
-		temp[(i + arr.length + arr2.length)] = arr3[i];
-	}	
-	
-	//iterate through the temp array to find 1-9.
-    for (i = 0; i < exists.length; i++) {
-        for (j = 0; j < temp.length; j++) {
-            if (temp[j] == i + 1) {
-				exists[i] = true;
+    public static void movesRemaining(int[] arr, int[] arr2, int[] arr3) {
+    	
+        int[] temp = new int[(arr.length + arr2.length + arr3.length)];
+        int i;
+        int j;
+        boolean[] exists = {false, false, false, false, false, false, false, false, false};
+    
+        //copy the subboard values to a temp array.
+        for (i = 0; i < arr.length; i++) {
+            temp[i] = arr[i];
+        }
+        
+        //add the column values to the temp array.
+        for (i = 0; i < arr2.length; i++) {
+            temp[(i + arr.length)] = arr2[i];
+        }
+        
+        //add the row values to the temp array.
+        for (i = 0; i < arr3.length; i++) {
+            temp[(i + arr.length + arr2.length)] = arr3[i];
+        }	
+        
+        //iterate through the temp array to find 1-9.
+        for (i = 0; i < exists.length; i++) {
+            for (j = 0; j < temp.length; j++) {
+                if (temp[j] == i + 1) {
+                    exists[i] = true;
+                }
             }
         }
-	}
-	
-	//print the missing values
-   	for (i = 0; i < exists.length; i++) {
-		if (exists[i] == false) {
-			System.out.print((i + 1) + " ");
-		}
-	}
+        
+        //print the missing values
+        for (i = 0; i < exists.length; i++) {
+            if (exists[i] == false) {
+                System.out.print((i + 1) + " ");
+            }
+        }
+    }
 }
